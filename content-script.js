2e0b1a2a3fe62f014ae357d5ded012e5df5522f8
knightron0 <sarthak.robo@gmail.com>
2022-12-25 01:22:46 +0530

    (feat) add saving in-browser

--- content-script.js
+++ content-script.js
@@ -1,3 +1,13 @@
+let config = {
+    'views': false,
+    'explore': true, 
+    'bookmarks': true, 
+    'lists': true,
+    'messages': true, 
+    'happening': true,
+    'whotofollow': true
+}
+
 let observer = new MutationObserver(callback);
     
 function callback (mutations) {
@@ -16,3 +26,62 @@ function callback (mutations) {
 }
 
 observer.observe(document.getElementById("react-root"), {subtree: true, childList: true});
+
+chrome.storage.local.get(['views', 'explore', 'bookmarks', 'lists', 'messages', 'happening', 'whotofollow'], function(result) {
+    config.views = result.views;
+    config.explore = result.explore;
+    config.bookmarks = result.bookmarks;
+    config.lists = result.lists;
+    config.messages = result.messages;
+    config.happening = result.happening;
+    config.whotofollow = result.whotofollow;
+});
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.hideViews) {
+            config.views = !config.views;
+            chrome.storage.local.set({'views': config.views}, function() {});
+            sendResponse({status: true});
+        }
+
+        if (request.hideExplore) {
+            config.explore = !config.explore;
+            chrome.storage.local.set({'explore': config.explore}, function() {});
+            sendResponse({status: true});
+        }
+
+        if (request.hideBookmarks) {
+            config.bookmarks = !config.bookmarks;
+            chrome.storage.local.set({'bookmarks': config.bookmarks}, function() {});
+            sendResponse({status: true});
+        }
+        
+        if (request.hideLists) {
+            config.lists = !config.lists;
+            chrome.storage.local.set({'lists': config.lists}, function() {});
+            sendResponse({status: true});
+        }
+
+        if (request.hideMessages) {
+            config.messages = !config.messages;
+            chrome.storage.local.set({'messages': config.messages}, function() {});
+            sendResponse({status: true});
+        }
+        
+        if (request.hideHappening) {
+            config.happening = !config.happening;
+            chrome.storage.local.set({'happening': config.happening}, function() {});
+            sendResponse({status: true});
+        }
+
+        if (request.hideWhoToFollow) {
+            config.whotofollow = !config.whotofollow;
+            chrome.storage.local.set({'whotofollow': config.whotofollow}, function() {});
+            sendResponse({status: true});
+        }
+
+        return true;
+    }
+);
--- popup.js
+++ popup.js
@@ -15,27 +15,66 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // removing tweet views by default
-      
     
-    // var toggleComments = document.getElementById("toggleComments");
-    // toggleComments.addEventListener('click', function(){
-    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //         chrome.tabs.sendMessage(tabs[0].id, {toggleComments: true}, function(response) {});
-    //     });
-    // })
-    // var toggleLiveComments = document.getElementById("toggleLiveComments");
-    // toggleLiveComments.addEventListener('click', function(){
-    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //         chrome.tabs.sendMessage(tabs[0].id, {toggleLiveComments: true}, function(response) {});
-    //     });
-    // })
-    // var slider = document.getElementById("densitySlider");
-    // var output = document.getElementById("valueDisplay");
-    // slider.oninput = function() {
-    //     output.innerHTML = this.value.toString() + ((this.value == 1) ? " second" : " seconds");
-    //     const val = this.value.toString()
-    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //         chrome.tabs.sendMessage(tabs[0].id, {densityChange: true, densityValue: val}, function(response) {});
-    //     });
-    // }
+    const hideViews = document.getElementById("hide-views");
+    hideViews.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideViews: true}, function(response) {});
+        })
+    })
+
+    const hideExplore = document.getElementById("hide-explore");
+    hideExplore.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideExplore: true}, function(response) {});
+        })
+    })
+
+    const hideBookmarks = document.getElementById("hide-bookmarks");
+    hideBookmarks.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideBookmarks: true}, function(response) {});
+        })
+    })
+    
+    const hideLists = document.getElementById("hide-lists");
+    hideLists.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideLists: true}, function(response) {});
+        })
+    })
+
+    const hideMessages = document.getElementById("hide-messages");
+    hideMessages.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideMessages: true}, function(response) {});
+        })
+    })
+
+    const hideHappening = document.getElementById("hide-whats-happening");
+    hideHappening.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideHappening: true}, function(response) {});
+        })
+    })
+
+    const hideWhoToFollow = document.getElementById("hide-who-to-follow");
+    hideWhoToFollow.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {hideWhoToFollow: true}, function(response) {});
+        })
+    })    
 });
+
+
+window.onload = function() {
+    chrome.storage.local.get(['views', 'explore', 'bookmarks', 'lists', 'messages', 'happening', 'whotofollow'], function(result) {
+        document.getElementById("hide-views").checked = !result.views
+        document.getElementById("hide-explore").checked = !result.explore
+        document.getElementById("hide-bookmarks").checked = !result.bookmarks
+        document.getElementById("hide-lists").checked = !result.lists
+        document.getElementById("hide-messages").checked = !result.messages
+        document.getElementById("hide-whats-happening").checked = !result.happening
+        document.getElementById("hide-who-to-follow").checked = !result.whotofollow
+    });
+}
